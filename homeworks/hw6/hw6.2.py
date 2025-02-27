1ecc1110fe3aa227fb72732bd00c6ddaa50d5d14
Gellershtein <s.zayats@tut.by>
2025-02-27 19:09:37 +0300

    small changes hw6.2

--- homeworks/hw6/hw6.2.py
+++ homeworks/hw6/hw6.2.py
@@ -25,11 +25,11 @@ while True:
 # print(f"Введенное трёхзначное число: {three_digit_number}")
 # print(f"Вычисляем числа по разрядам в числе {three_digit_number}")
 
-first_digit_number = three_digit_number//100
+first_digit = three_digit_number // 100
 two_digit_number = (three_digit_number%100)
-second_digit_number = two_digit_number//10
-third_digit_number = two_digit_number%10
-print(f"Введенное трёхзначное число: {three_digit_number}, первый разряд = {first_digit_number} второй разряд = {second_digit_number}  трейтий разряд = {third_digit_number}")
-three_digit_numbers_sum = MathUtils.sum(MathUtils.sum(first_digit_number, second_digit_number), third_digit_number)
-three_digit_numbers_multipling = MathUtils.multiply(MathUtils.multiply(first_digit_number, second_digit_number), third_digit_number)
+second_digit = two_digit_number//10
+third_digit = two_digit_number % 10
+print(f"Разложение числа: {three_digit_number} на разряды: первый разряд = {first_digit} второй разряд = {second_digit}  трейтий разряд = {third_digit}")
+three_digit_numbers_sum = MathUtils.sum(MathUtils.sum(first_digit, second_digit), third_digit)
+three_digit_numbers_multipling = MathUtils.multiply(MathUtils.multiply(first_digit, second_digit), third_digit)
 print(f"Вычисляем сумму и произведение цифр трёхзначного числа {three_digit_number}, Сумма чисел = {three_digit_numbers_sum}, Произведение чисел = {three_digit_numbers_multipling}")
