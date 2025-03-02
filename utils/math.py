2624674a9615dee9118534bd723dfa33eb9e70d3
Gellershtein <s.zayats@tut.by>
2025-03-02 14:06:37 +0300

    change comments type

--- utils/math.py
+++ utils/math.py
@@ -1,28 +1,34 @@
 class MathUtils:
     @staticmethod
-    def sum(a, b): #Сложение
+    def sum(a, b):
+        """Сложение"""
         return a + b
 
     @staticmethod
-    def difference(a, b): #Вычитание
+    def difference(a, b):
+        """ Вычитание """
         return a - b
 
     @staticmethod
-    def multiply(a, b): #Умножение
+    def multiply(a, b):
+        """Умножение"""
         return a * b
 
     @staticmethod
-    def divide(a, b): #Деление
+    def divide(a, b):
+        """Деление"""
         if b == 0:
             raise ValueError("Деление на ноль недопустимо")
         return a / b
 
     @staticmethod
-    def mod(a, b):#Деление по модулю (остаток от деления)
+    def mod(a, b):
+        """Деление по модулю (остаток от деления)"""
         if b == 0:
             raise ValueError("Деление на ноль недопустимо")
         return a % b
 
     @staticmethod
-    def discriminant(a, b, c): #Вычисление дискриминанта
+    def discriminant(a, b, c):
+        """Вычисление дискриминанта"""
         return b**2 - 4*a*c
